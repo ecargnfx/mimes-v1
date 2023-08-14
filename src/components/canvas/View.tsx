9a9fcc00b7e5129e3f72b8be0130c4768e9ed893
Grace Ng <grace@leanstartupmachine.com>
2023-08-14 12:03:38 -0700

    remove ambient light error

--- src/components/canvas/View.tsx
+++ src/components/canvas/View.tsx
@@ -8,7 +8,6 @@ import { Three } from '@/helpers/components/Three'
 export const Common = ({ color }) => (
   <Suspense fallback={null}>
     {color && <color attach='background' args={[color]} />}
-    <ambientLight intensity={0.5} />
     <pointLight position={[20, 30, 10]} intensity={1} />
     <pointLight position={[-10, -10, -10]} color='blue' />
     <PerspectiveCamera makeDefault fov={40} position={[0, 0, 6]} />
