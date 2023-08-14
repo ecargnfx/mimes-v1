d8a7faf168a7dc9a02a65a0004db2491630b227a
Grace Ng <grace@leanstartupmachine.com>
2023-08-14 12:49:16 -0700

    define addition PerspectiveCamera props

--- src/components/canvas/View.tsx
+++ src/components/canvas/View.tsx
@@ -5,6 +5,25 @@ import { OrbitControls, PerspectiveCamera, View as ViewImpl } from '@react-three
 import { Three } from '@/helpers/components/Three'
 
 
+type CustomPerspectiveCameraProps = Partial<React.ComponentProps<typeof PerspectiveCamera>>;
+
+export const CustomPerspectiveCamera: React.FC<CustomPerspectiveCameraProps> = ({
+  makeDefault = true,
+  fov = 40,
+  position = [0, 0, 6],
+  ...props
+}) => {
+  return (
+    <PerspectiveCamera
+      makeDefault={makeDefault}
+      fov={fov}
+      position={position}
+      {...props}
+    />
+  );
+};
+
+
 export const Common = ({ color }) => (
   <Suspense fallback={null}>
     {color && <color attach='background' args={[color]} />}
