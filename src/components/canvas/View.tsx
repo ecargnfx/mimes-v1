351d249e47c580f3390838c32fadde09b8abdbe2
Grace Ng <grace@leanstartupmachine.com>
2023-08-14 12:14:54 -0700

    fix pointlight intensity error

--- src/components/canvas/View.tsx
+++ src/components/canvas/View.tsx
@@ -8,7 +8,7 @@ import { Three } from '@/helpers/components/Three'
 export const Common = ({ color }) => (
   <Suspense fallback={null}>
     {color && <color attach='background' args={[color]} />}
-    <pointLight position={[20, 30, 10]} intensity={1} />
+    <pointLight position={[20, 30, 10]} />
     <pointLight position={[-10, -10, -10]} color='blue' />
     <PerspectiveCamera makeDefault fov={40} position={[0, 0, 6]} />
   </Suspense>
