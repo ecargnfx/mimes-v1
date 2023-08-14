ec91d2d6ebfc644fd66bf62b7e5e54075c006ea2
Grace Ng <grace@leanstartupmachine.com>
2023-08-14 12:19:40 -0700

    fix pointlight color error

--- src/components/canvas/View.tsx
+++ src/components/canvas/View.tsx
@@ -9,7 +9,7 @@ export const Common = ({ color }) => (
   <Suspense fallback={null}>
     {color && <color attach='background' args={[color]} />}
     <pointLight position={[20, 30, 10]} />
-    <pointLight position={[-10, -10, -10]} color='blue' />
+    <pointLight position={[-10, -10, -10]} />
     <PerspectiveCamera makeDefault fov={40} position={[0, 0, 6]} />
   </Suspense>
 )
